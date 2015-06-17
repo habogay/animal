e924d593edcd2ff47aed8654b9ae737cabc4a374
habogay <habogay@gmail.com>
2015-06-17 10:51:47 +0700

    add src folder

---
+++ src/com/ohm/entity/Animal.java
@@ -0,0 +1,18 @@
+package com.ohm.entity;
+
+public class Animal {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String sound()
+	{
+		
+		return "WOW WOW";
+	}
+}
